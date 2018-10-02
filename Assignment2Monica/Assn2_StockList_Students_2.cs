1ebf1ea905a6261a1c001ae8a12914a9e5248a48
anuragkunchamwar1 <43157808+anuragkunchamwar1@users.noreply.github.com>
2018-10-01 22:57:46 -0400

    student2

---
+++ Assignment2Monica/Assn2_StockList_Students_2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+namespace Assignment_2
+{
+  public partial class StockList
+  {
+    //param   (StockList)listToMerge : second list to be merged 
+    //summary      : merge two different list into a single result list
+    //return       : merged list
+    //return type  : StockList
+    public StockList MergeList(StockList listToMerge)
+    {
+      StockList resultList = new StockList();
+
+      // write your implementation here
+      //merging two lists using the while conditional statement
+      StockNode node = this.head;
+      while(node != null)
+		{
+			resultList.AddLast(node.StockHolding);
+			node = node.Next;
+		}
+		node = listToMerge.head;
+		while(node != null)
+		{
+			resultList.AddLast(node.StockHolding);
+			node = node.Next;
+		}
+		
+      return resultList;
+    }
+
+    //param        : NA
+    //summary      : finds the stock with most number of holdings
+    //return       : stock with most shares
+    //return type  : Stock
+    public Stock MostShares()
+    {
+      Stock mostShareStock = null;
+
+      StockNode stock = this.head;
+      decimal maxHoldings = 0.0m;
+      
+      while(stock != null)
+      {
+      	if(stock.StockHolding.Holdings > maxHoldings)
+      	{
+      		maxHoldings = stock.StockHolding.Holdings;
+      		mostShareStock = stock.StockHolding;
+      	}
+      	stock = stock.Next;
+      }
+     
+      return mostShareStock;
+    }
+
+    //param        : NA
+    //summary      : finds the number of nodes present in the list
+    //return       : length of list
+    //return type  : int
+    public int Length()
+    {
+      int length = 0;
+
+     StockNode sNode = this.head;
+     
+     while(sNode != null)
+     {
+     	length++;
+     	sNode = sNode.Next;
+     }
+      return length;
+    }
+  }
+}
